734e2686f6480d12f6d2065e9644c2f120c35b9d
mazzia (Prof. ) <francesca.mazzia@uniba.it>
2023-12-09 22:31:01 +0000

    changed uround in mebdfi, deleted d1mach and changed errmsg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bvpsolve@327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/deTestSet/src/errmsg.f
+++ pkg/deTestSet/src/errmsg.f
@@ -1,101 +1,113 @@
 c ===================================================================================
 c print R-messages
+c FM 02/11/2023  changed character*(*)  with character(:), allocatable
+c FM 02/11/2023 changed label//char(0) into label
 c ===================================================================================
-
+c
       subroutine intpr_k(label, nchar, data, ndata)
       integer nchar, ndata
-      character*(*) label
+c      character*(*) label
+      character(:), allocatable :: label
       integer data(ndata)
 
-       if (ndata .eq. 1) then
-         call rprintfi1(label//char(0), data(1))
-       else if (ndata .eq. 2) then
-         call rprintfi2(label//char(0), data(1), data(2))
-       else if (ndata .eq. 3) then
-         call rprintfi3(label//char(0), data(1), data(2), data(3))
-       else if (ndata .ge. 4) then
-         call rprintfi4(label//char(0),data(1),data(2),data(3),data(4))
-       endif 
+      if (ndata .eq. 1) then
+        call rprintfi1(label//char(0), data(1))
+      else if (ndata .eq. 2) then
+        call rprintfi2(label//char(0), data(1), data(2))
+      else if (ndata .eq. 3) then
+        call rprintfi3(label//char(0), data(1), data(2), data(3))
+      else if (ndata .ge. 4) then
+       call rprintfi4(label//char(0),data(1),data(2),data(3),data(4))
+      endif
       end subroutine
 
 
       subroutine dblepr_k(label, nchar, data, ndata)
       integer nchar, ndata
-      character*(*) label
+c      character*(*) label
+      character(:), allocatable :: label
       double precision data(ndata)
 
-       if (ndata .eq. 1) then
-         call rprintfd1(label//char(0), data(1))
-       else if (ndata .eq. 2) then
-         call rprintfd2(label//char(0), data(1), data(2))
-       else if (ndata .eq. 3) then
-         call rprintfd3(label//char(0), data(1), data(2), data(3))
-       else if (ndata .ge. 4) then
-         call rprintfd4(label//char(0),data(1),data(2),data(3),data(4))
-       endif 
+      if (ndata .eq. 1) then
+        call rprintfd1(label//char(0), data(1))
+      else if (ndata .eq. 2) then
+        call rprintfd2(label//char(0), data(1), data(2))
+      else if (ndata .eq. 3) then
+        call rprintfd3(label//char(0), data(1), data(2), data(3))
+      else if (ndata .ge. 4) then
+        call rprintfd4(label//char(0),data(1),data(2),data(3),data(4))
+      endif
       end subroutine
 
 C just a string
       subroutine rprint(msg)
-      character (len=*) msg
-
+c      character (len=*) msg
+      character(:), allocatable :: msg
       call rprintf(msg//char(0))
 
-      end subroutine 
+      end subroutine
 
 
 C printing with one integer and a double
       subroutine rprintid(msg, i1, d1)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       double precision d1
       integer i1
-         call rprintfd1(msg//char(0), d1)
-         call rprintfi1(' '//char(0), i1)
-      end subroutine 
+      call rprintfd1(msg//char(0), d1)
+      call rprintfi1(' '//char(0), i1)
+      end subroutine
 
 
 C printing with doubles
 C
       subroutine rprintd1(msg, d1)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       double precision d1
-        call rprintfd1(msg//char(0), d1)
-      end subroutine 
+      call rprintfd1(msg//char(0), d1)
+      end subroutine
 C
       subroutine rprintd2(msg, d1, d2)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       double precision d1, d2
-        call rprintfd2(msg//char(0), d1, d2)
-      end subroutine 
+      call rprintfd2(msg//char(0), d1, d2)
+      end subroutine
 C
       subroutine rprintd3(msg, d1, d2, d3)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       double precision d1, d2, d3
-        call rprintfd3(msg//char(0), d1, d2, d3)
-      end subroutine 
+      call rprintfd3(msg//char(0), d1, d2, d3)
+      end subroutine
 C
       subroutine rprintd4(msg, d1, d2, d3, d4)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       double precision d1, d2, d3, d4
-        call rprintfd4(msg//char(0), d1, d2, d3, d4)
-      end subroutine 
+      call rprintfd4(msg//char(0), d1, d2, d3, d4)
+      end subroutine
 
 
 C printing with integers
       subroutine rprinti1(msg, i1)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       integer i1
-        call rprintfi1(msg//char(0), i1)
-      end subroutine 
+      call rprintfi1(msg//char(0), i1)
+      end subroutine
 C
       subroutine rprinti2(msg, i1, i2)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       INTEGER i1, i2
-        call rprintfi2(msg//char(0), i1, i2)
-      end subroutine 
+      call rprintfi2(msg//char(0), i1, i2)
+      end subroutine
 C
       subroutine rprinti3(msg, i1, i2, i3)
-      character (len=*) msg
+c      character (len=*) msg
+      character(:), allocatable :: msg
       INTEGER i1, i2, i3
-        call rprintfi3(msg//char(0), i1, i2, i3)
+      call rprintfi3(msg//char(0), i1, i2, i3)
       end subroutine
